fc832904a1d244d9675cfcf0cc091ac5db05a044
jsooter <jsooter.dev@gmail.com>
2021-10-23 14:27:13 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+    const validEmailsUrl = core.getInput('valid-emails-url');
+
+    request(validEmailsUrl, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        
+        console.log(res);
+
+        committer_email = github.context.payload.head_commit.committer.email;
+
+        if (committer_email != 'josh-sooter@pluralsight.com') {
+
+            core.setFailed(`Committ email ${committer_email} is is not compliant`);
+        }
+
+        console.log(`Committer email: ${committer_email}`);
+    });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
